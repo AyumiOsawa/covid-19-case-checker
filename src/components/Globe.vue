238879d225c493170ed026e1eb840a3acac774d6
AyumiOsawa <a.osawa1002@gmail.com>
2021-04-09 11:19:25 +0200

    sitemap added

---
+++ src/components/Globe.vue
@@ -0,0 +1,22 @@
+<template>
+  Globe
+  data {{basicData}}
+  <div id="globe"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Globe',
+  props: {
+    basicData: [],
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+#globe {
+  background-color: azure;
+}
+</style>
